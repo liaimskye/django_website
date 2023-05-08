a9a0b3805c03d20124e3eef5e36b3355e7374309
liaim Reeves <liaimskye@gmail.com>
2023-05-08 13:04:20 +0200

    docstrings added to inventory models

--- capstone_site/inventory/models.py
+++ capstone_site/inventory/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 from django.conf import settings
 # Create your models here.
 class Stock(models.Model):
+    """Creates inventory item objects and stores them in the database.
+    
+
+Attributes:
+inventory_items(Charfield):The names of inventory items.
+
+Modules:
+__str__(self): returns the string form of stock objects.
+"""
     inventory_items = models.CharField(max_length=100)
 
     def __str__(self):
@@ -13,5 +22,3 @@ class Query(models.Model):
     name = models.CharField(("Name"), max_length=50)
     email_address = models.EmailField(unique=False, null=True)
     date_created = models.DateTimeField(auto_now=True)
-
-    
